a183e019538d45e869d247d708af49a2ed0e53e5
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2017-01-24 22:03:33 -0200

    bump to 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="airflow_utils",
-    version="0.1",
+    version="0.3",
     description="collection of helpers to create airflow dags",
     author="Big Data Team at globo.com",
     author_email="tarsis@corp.globo.com",
